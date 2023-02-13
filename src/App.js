a3fbf906e2df5b04005c29804962e02bca1f949a
Philipp Kuzyanin <phkuzyanin@gmail.com>
2023-02-14 01:10:24 +0300

    added form fields layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React from 'react'
-import Form from './components/Form'
+import { useForm } from 'react-hook-form'
 function App() {
+  
+  const {
+    register,   //метод, который позволяет регистрировать разные поля для формы
+    formState:{ errors },
+    handleSubmit, //обертка над кастомными хэндлерами
+  } = useForm()
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data))
+  } // метод onSubmit получает данные из формы с помощью handleSubmit, но орабатывается только, если в форме нет ошибок
+  const styles ={
+    form:{
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      flexGrow:'0',
+    },
+    label :{
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      flexGrow:'0',
+      marginBottom:'1em'
+    }
+  }
   return (
-  <Form />
+    <>
+      <h1>React Hook Form</h1>
+      <form onSubmit ={handleSubmit(onSubmit)} style={styles.label}>
+        <label style={styles.label}>
+          <input {...register('firstName',{required:true})} placeholder='First Name'/>
+          <input {...register('lastName',{required:true})} placeholder='Last Name'/>
+          <input {...register('gender',{required:true})} placeholder='Gender'/>
+          <input {...register('phoneNumber',{required:true})} placeholder='Phone Number'/>
+          <input {...register('email',{required:true})} placeholder='E-mail'/>
+          <input {...register('password',{required:true})} placeholder='Password'/>
+          <input {...register('passwordConfirm',{required:true})} placeholder='Confirm Password'/>
+        </label>
+        <input type="submit" value="SUBMIT" />
+      </form>
+    </>
   );
 }
 
